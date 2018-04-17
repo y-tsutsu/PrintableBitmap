9b765e364692868982de6a446cb56652fa149fc3
tsutsu <tsutsu0928@gmail.com>
2018-04-17 20:31:59 +0900

    PrintableBitmapを作成．

---
+++ PrintableBitmap/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PrintableBitmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bmp = new PrintableBitmap(args[0]);
+            System.Console.WriteLine(bmp);
+        }
+    }
+}
